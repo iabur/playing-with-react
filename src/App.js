aa4bdf86ae7b2a7c9192ff07cd89c9a2c36eaa90
iaburrahman <iaburrahman@gmail.com>
2020-09-03 21:00:56 +0600

    convert function to class component and define state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Hello, This is first react app</h1>
-      <Person name='iabur' age='26' />
-      <Person name='rohim' age='35'>
-        I am a shopkeeper
-      </Person>
-    </div>
-  );
+class App extends Component {
+  state = {
+    Person: [
+      { name: 'iabur', age: '26' },
+      { name: 'rohim', age: 30 },
+      { name: 'korim', age: 33 },
+    ],
+  };
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hello, This is first react app</h1>
+        <Person
+          name={this.state.Person[0].name}
+          age={this.state.Person[0].age}
+        />
+        <Person name={this.state.Person[1].name} age={this.state.Person[1].age}>
+          I am a shopkeeper
+        </Person>
+      </div>
+    );
+  }
 }
 
 export default App;
